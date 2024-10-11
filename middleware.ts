584e34f2ab75192873f3913c1c5c12b6702be38c
dharmik48 <hingudharmik4@gmail.com>
2024-10-11 23:19:12 +0530

    feat: adds middleware authorization

---
+++ middleware.ts
@@ -0,0 +1,14 @@
+import type { NextRequest } from 'next/server'
+import { getLoggedInUser } from './lib/actions/patient.actions'
+
+export async function middleware(request: NextRequest) {
+	const { user, error } = await getLoggedInUser()
+
+	if (!user && !request.nextUrl.pathname.startsWith('/login')) {
+		return Response.redirect(new URL('/login', request.url))
+	}
+}
+
+export const config = {
+	matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
+}
