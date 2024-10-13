3cd4aa8eec82e72f12b7aa393f2c1a4c3b1b5807
dharmik48 <hingudharmik4@gmail.com>
2024-10-13 13:03:26 +0530

    feat: moves appointment pages to new patient page

--- app/patient/[id]/layout.tsx
+++ app/patient/[id]/layout.tsx
@@ -21,6 +21,10 @@ export default function SettingsLayout({
 			title: 'Person Details',
 			href: `/patient/${id}/details`,
 		},
+		{
+			title: 'New Appointment',
+			href: `/patient/${id}/new-appointment`,
+		},
 	]
 
 	return (
@@ -41,7 +45,7 @@ export default function SettingsLayout({
 					className='hidden dark:block'
 				/>
 			</div> */}
-			<div className='hidden space-y-6 p-10 pb-16 md:block'>
+			<div className='hidden space-y-6 p-10 md:block'>
 				<div className='space-y-0.5'>
 					<h2 className='text-2xl font-bold tracking-tight'>Settings</h2>
 					<p className='text-muted-foreground'>
---
+++ app/patient/[id]/new-appointment/page.tsx
@@ -0,0 +1,24 @@
+import AppointmentForm from '@/components/AppointmentForm'
+import { getPatientByUserId } from '@/lib/actions/patient.actions'
+import * as Sentry from '@sentry/nextjs'
+
+const NewAppointment = async ({ params: { id } }: SearchParamProps) => {
+	const { patient } = await getPatientByUserId(id)
+	Sentry.metrics.set('user_view_new-appointment', patient.name)
+
+	return (
+		<section className='h-full'>
+			<AppointmentForm
+				type='create'
+				userId={id}
+				patientId={patient.$id}
+				doctor={patient.primaryPhysician}
+			/>
+			<div className='flex justify-between text-14-regular items-center mt-8'>
+				<p className='copyright'>&copy;carepulse {new Date().getFullYear()}</p>
+			</div>
+		</section>
+	)
+}
+
+export default NewAppointment
