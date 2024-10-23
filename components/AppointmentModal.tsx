15604d43e4816a91693c43b298100a308293c91c
dharmik48 <hingudharmik4@gmail.com>
2024-10-23 21:47:08 +0530

    sytle: touch up table colors

--- components/AppointmentModal.tsx
+++ components/AppointmentModal.tsx
@@ -1,66 +1,67 @@
 'use client'
 
 import {
-	Dialog,
-	DialogContent,
-	DialogDescription,
-	DialogHeader,
-	DialogTitle,
-	DialogTrigger,
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
 } from '@/components/ui/dialog'
 import clsx from 'clsx'
 import AppointmentForm from './AppointmentForm'
-import { Appointment } from '@/types/appwrite.types'
-import { useState } from 'react'
-import { Button } from './ui/button'
+import {Appointment} from '@/types/appwrite.types'
+import {useState} from 'react'
+import {Button} from './ui/button'
 
 interface Props {
-	type: 'schedule' | 'cancel'
-	patientId: string
-	userId: string
-	appointment: Appointment
-	disabled?: boolean
+    type: 'schedule' | 'cancel'
+    patientId: string
+    userId: string
+    appointment: Appointment
+    disabled?: boolean
 }
 
 const AppointmentModal = ({
-	type,
-	appointment,
-	patientId,
-	userId,
-	disabled,
-}: Props) => {
-	const [open, setOpen] = useState(false)
+                              type,
+                              appointment,
+                              patientId,
+                              userId,
+                              disabled,
+                          }: Props) => {
+    const [open, setOpen] = useState(false)
 
-	return (
-		<Dialog open={open} onOpenChange={setOpen}>
-			<DialogTrigger asChild>
-				<Button
-					variant={'ghost'}
-					className={clsx('capitalize', {
-						'text-green-500': type === 'schedule',
-					})}
-					disabled={disabled}
-				>
-					{type}
-				</Button>
-			</DialogTrigger>
-			<DialogContent className='shad-dialog sm:max-w-md'>
-				<DialogHeader className='mb-4 space-y-3'>
-					<DialogTitle className='capitalize'>{type} appointment</DialogTitle>
-					<DialogDescription>
-						Please fill in the following details to {type} appointment
-					</DialogDescription>
-				</DialogHeader>
-				<AppointmentForm
-					type={type}
-					patientId={patientId}
-					userId={userId}
-					appointment={appointment}
-					setOpen={setOpen}
-				/>
-			</DialogContent>
-		</Dialog>
-	)
+    return (
+        <Dialog open={open} onOpenChange={setOpen}>
+            <DialogTrigger asChild>
+                <Button
+                    variant={'ghost'}
+                    className={clsx('capitalize', {
+                        'text-green-500': type === 'schedule',
+                        'hover:bg-destructive hover:text-destructive-foreground': type === 'cancel'
+                    })}
+                    disabled={disabled}
+                >
+                    {type}
+                </Button>
+            </DialogTrigger>
+            <DialogContent className='shad-dialog sm:max-w-md'>
+                <DialogHeader className='mb-4 space-y-3'>
+                    <DialogTitle className='capitalize'>{type} appointment</DialogTitle>
+                    <DialogDescription>
+                        Please fill in the following details to {type} appointment
+                    </DialogDescription>
+                </DialogHeader>
+                <AppointmentForm
+                    type={type}
+                    patientId={patientId}
+                    userId={userId}
+                    appointment={appointment}
+                    setOpen={setOpen}
+                />
+            </DialogContent>
+        </Dialog>
+    )
 }
 
 export default AppointmentModal
